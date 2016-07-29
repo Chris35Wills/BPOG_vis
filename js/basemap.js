1c6618f01a021e4bb5b09d3c60ffe317ba174bbd
CN Williams <cw14910@bristol.ac.uk>
2016-07-29 10:37:59 +0100

    switched to nasa wms blue marble

--- js/basemap.js
+++ js/basemap.js
@@ -11,16 +11,18 @@ var map = L.mapbox.map('map')
 
 // add the default map for the app (a map from mapbox (outdoor map with text)) - additional layers will be added later
 //var overviewMap = L.tileLayer('https://api.mapbox.com/styles/v1/chriswills/cir23gikh000ljcnn13ee2y6g/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hyaXN3aWxscyIsImEiOiJjaXFyeTdhNmgwMDl3aTRubTc2aXZueXhsIn0.ULMwLML50j_FGwrRAWNpEw', {
-    isBaseLayer: true
+//    isBaseLayer: true
 //});
 //overviewMap.addTo(map);
 
 // add the bluemarble satellite image as the basemap 
-var wmsLayer = L.tileLayer.wms('http://demo.opengeo.org/geoserver/ows?', {
-    layers: 'nasa:bluemarble'
-}).addTo(map);
+//var wmsLayer = L.tileLayer.wms('http://demo.opengeo.org/geoserver/ows?', {
+//    layers: 'nasa:bluemarble'
+//}).addTo(map);
 
-//var wmsLayer = L.tileLayer.wms('http://svs.gsfc.nasa.gov/cgi-bin/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=2915_21223&FORMAT=image/png&WIDTH=1024&HEIGHT=512&CRS=CRS:84&BBOX=-180.0,-90.0,180.0,90.0&STYLES=').addTo(map);
+var wmsLayer = L.tileLayer.wms('http://svs.gsfc.nasa.gov/cgi-bin/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=2915_21223&FORMAT=image/png&WIDTH=1024&HEIGHT=512&CRS=CRS:84&BBOX=-180.0,-90.0,180.0,90.0&STYLES=', {
+    layers: 'Complete Earth (2048x1024 Image)'
+}).addTo(map);
 
 //L.tileLayer('https://api.mapbox.com/styles/v1/chriswills/cir23gikh000ljcnn13ee2y6g/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2hyaXN3aWxscyIsImEiOiJjaXFyeTdhNmgwMDl3aTRubTc2aXZueXhsIn0.ULMwLML50j_FGwrRAWNpEw',{isBaseLayer: true}).addTo(map)
 
